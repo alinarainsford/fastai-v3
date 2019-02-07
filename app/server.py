9eb4f9eb27f6d225e47d799baca40fcd8c255cdb
alinarainsford <alinarainsford@earthlink.net>
2019-02-07 14:15:16 -0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -8,7 +8,7 @@ from io import BytesIO
 from fastai import *
 from fastai.vision import *
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=1r5_QCuXaIaThOnxrq3xh5XzYF_vxyvxV'
+export_file_url = 'https://drive.google.com/uc?export=download&id=ttps://drive.google.com/open?id=1La4JKuZLkcrjpkDNnRG0FRADKJVyhMN'
 export_file_name = 'export.pkl'
 
 classes = ['sharks', 'dolphins', 'mantarays']
